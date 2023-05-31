0e958357261fc0b42d2a2c0fbb0c2da6f13cb601
rsxswp <134602073+rsxswp@users.noreply.github.com>
2023-05-31 08:09:36 +0700

    Update 20200119085848-create-status.js

--- user-status-api/app/migrations/20200119085848-create-status.js
+++ user-status-api/app/migrations/20200119085848-create-status.js
@@ -1,32 +1,35 @@
-'use strict';
+"use strict";
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Statuses', {
+    return queryInterface.createTable("Statuses", {
       id: {
         allowNull: false,
         primaryKey: true,
-        type: Sequelize.UUID
+        type: Sequelize.UUID,
       },
       title: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
       },
       body: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
       },
       user_id: {
-        type: Sequelize.STRING
+        allowNull: false,
+        type: Sequelize.STRING,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Statuses');
-  }
+    return queryInterface.dropTable("Statuses");
+  },
 };
